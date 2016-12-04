8659e628c56468654d5ac55e03962466fd79bc4a
WillCurran <willecurran@gmail.com>
2016-12-04 16:21:42 -0600

    1D encoder PID 1st attempt

--- src/org/usfirst/frc/team2850/robot/Robot.java
+++ src/org/usfirst/frc/team2850/robot/Robot.java
@@ -7,7 +7,9 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -26,10 +28,25 @@ public class Robot extends IterativeRobot {
 	public static Spark rightDrive1;
 	public static Spark leftDrive2;
 	public static Spark rightDrive2;
+	public static Spark leftDrive3;
+	public static Spark rightDrive3;
 	public static Compressor compressor;
 	public static Solenoid driveshifter;
-    public static boolean high;
+	public static Encoder leftEncoder;
+	public static Encoder rightEncoder;
 	
+    public static boolean high;
+    //how many pulses per rotation? how many feet per rotation? (gears, tires, CIM speed)
+    public static double DIST_PER_PULSE = .2;
+   	
+    public static double Kp = .005;
+    public static double Ki = .005;
+    public static double Kd = .005;
+    
+   	public static PIDController linearDriveLeft = new PIDController(Kp, Ki, Kd, leftEncoder, leftDrive1);
+   	public static PIDController linearDriveRight = new PIDController(Kp, Ki, Kd, rightEncoder, rightDrive1);
+   	
+   	public static double PID_SETPOINT = 10.0; //10 feet
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
@@ -46,7 +63,26 @@ public class Robot extends IterativeRobot {
     	compressor = new Compressor();
     	driveshifter=new Solenoid(0);
        
-        
+    	leftEncoder = new Encoder(4, 5, false, Encoder.EncodingType.k4X);
+    	rightEncoder = new Encoder(6, 7, false, Encoder.EncodingType.k4X);
+    	
+    	leftEncoder.setMaxPeriod(.1);
+    	leftEncoder.setMinRate(10);
+    	leftEncoder.setDistancePerPulse(DIST_PER_PULSE);
+    	leftEncoder.setReverseDirection(false);
+    	leftEncoder.setSamplesToAverage(7);
+    	rightEncoder.setMaxPeriod(.1);
+    	rightEncoder.setMinRate(10);
+    	rightEncoder.setDistancePerPulse(DIST_PER_PULSE);
+    	rightEncoder.setReverseDirection(true);
+    	rightEncoder.setSamplesToAverage(7);
+    	
+    	linearDriveLeft.setAbsoluteTolerance(.5);
+    	linearDriveRight.setAbsoluteTolerance(.5);
+    	linearDriveLeft.setOutputRange(-1, 1);
+    	linearDriveLeft.setSetpoint(PID_SETPOINT);
+    	linearDriveRight.setOutputRange(-1, 1);
+    	linearDriveRight.setSetpoint(PID_SETPOINT);
     }
     
 	/**
@@ -58,15 +94,26 @@ public class Robot extends IterativeRobot {
 	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
 	 * If using the SendableChooser make sure to add them to the chooser code above as well.
 	 */
+    
+    
     public void autonomousInit() {
-    	
+    	linearDriveLeft.enable();
+    	linearDriveRight.enable();
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-    	
+    	//setting other drive motors to PID outputs
+    	leftDrive2.set(linearDriveLeft.get());
+    	rightDrive2.set(linearDriveRight.get());
+    	leftDrive3.set(linearDriveLeft.get());
+    	rightDrive3.set(linearDriveRight.get());
+    	if(linearDriveRight.onTarget())
+    		linearDriveRight.disable();
+    	if(linearDriveLeft.onTarget())
+    		linearDriveLeft.disable();
     }
 
     /**
